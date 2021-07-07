eac6c2e40fff9cb16f35aa13689377660362d55f
Bruce MacKinnon <bruce@mackinnon.com>
2021-07-07 16:11:58 -0400

    Adding variables

--- hello-parsimonious/main2.py
+++ hello-parsimonious/main2.py
@@ -13,29 +13,39 @@ grammar = Grammar(
     #
     #   temperature: Float
     #   temperature: Float @f(a: 1, b: { field1 { field2 }})
+    #   # Field arguments with some default values
     #   temperature(offset: Float = 0, scale: String = "f"): Float
     #
-    field_def         = field_name wsc? (lparen wsc? field_arg_list wsc? rparen)? wsc? colon wsc? 
+    field_def          = field_name wsc? (lparen wsc? field_def_arg_list wsc? rparen)? wsc? colon wsc? 
                         (scalar_type / array_type) wsc? declaration? wsc?
-    field_arg_list    = (field_arg wsc? comma wsc?)* field_arg
-    field_arg         = arg_name wsc? colon wsc? type_name wsc? ("=" wsc? constant)?
-    scalar_type       = type_name bang?
-    array_type        = lbracket wsc? type_name bang? wsc? rbracket bang?
-    declaration       = at_sign identifier wsc? lparen wsc? param_list? wsc? rparen wsc?
-    param_list        = (param wsc? comma wsc?)* param   
-    # Declaration parameter with optional default values
-    param             = identifier wsc? colon wsc? expr2 
-    expr2             = query / identifier / literal_number / literal_string
+    field_def_arg_list = (field_def_arg wsc? comma wsc?)* field_def_arg
+    field_def_arg      = arg_name wsc? colon wsc? type_name wsc? ("=" wsc? constant)?
+    scalar_type        = type_name bang?
+    array_type         = lbracket wsc? type_name bang? wsc? rbracket bang?
+    declaration        = at_sign identifier wsc? lparen wsc? param_list? wsc? rparen wsc?
+    param_list         = (param wsc? comma wsc?)* param   
+    # Declaration parameter passing
+    param              = identifier wsc? colon wsc? expr2 
+    expr2              = query / identifier / literal_number / literal_string / var_name
     
     # Query Language
-    query             = lbrace wsc? (selector wsc)+ rbrace wsc?
-    selector          = identifier wsc? query?
+    # { 
+    #    field1
+    #    field2 {
+    #      subfield1(a: 7, b: $u)
+    #    }
+    #  }    
+    query              = lbrace wsc? (field_sel wsc)+ rbrace wsc?
+    field_sel          = field_name wsc? (lparen wsc? field_sel_arg_list wsc? rparen)? wsc? query?
+    field_sel_arg_list = (field_sel_arg wsc? comma wsc?)* field_sel_arg
+    field_sel_arg      = arg_name wsc? colon wsc? (constant / var_name)
     
     # Fundamentals
     type_name         = ~"[A-Z_][A-Z_0-9]*"i
     field_name        = ~"[A-Z_][A-Z_0-9]*"i
     arg_name          = ~"[A-Z_][A-Z_0-9]*"i
     identifier        = ~"[A-Z_][A-Z_0-9]*"i
+    var_name          = "$" arg_name
     constant          = literal_string / literal_number
     literal_number    = ~"[0-9]+([.][0-9]+)?"
     # Match everything except quote and newline.  We need two backslashes so
@@ -74,7 +84,9 @@ schema {
       # A comment between fields.  Also demonstrating that the declaration 
       # can be split across lines
       name: String @derrived(a: 5, b: "henry!", 
-        c: 6 ,d : { test { a b } })
+        c: 6,
+        d: { test(fa: 7, bf: $passed_in) { a b } },
+        e: $another_passed_in)
       list: [Float!]
       list1(env: MarketEnv): [Float!]
       list2(env1: MarketEnv = 66, env2: MarketEnv = "test%4"): [Float!]
